2abec9e0b0066bff2c411429fe13943dcf9a0d10
jinhui36 <jinhui36@outlook.com>
2019-08-27 16:09:47 +0800

    fix error

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
 web: /bin/bash start.sh
-start: /bin/bash start.sh
-stop: /bin/bash stop.sh
--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-cd ghost
+cd "$HOME"
+chmod a+r ghost
+
+cd "$HOME/ghost"
 
 ghost config --port "$PORT" --url "http://localhost:$PORT" --dbpath "$HOME/ghost/content/data/ghost-local.db"
 ghost config paths.contentPath "$HOME/ghost/content"
-node current/index.js
