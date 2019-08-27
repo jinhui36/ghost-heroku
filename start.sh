03ec1e73b7d032e8e021ad92cb02b712a08da1a0
jinhui36 <jinhui36@outlook.com>
2019-08-27 15:56:25 +0800

    test

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-
+echo "test">test.txt
 cd ghost
 ln -s config.development.json config.production.json
 ghost config --port "$PORT" --url "http://localhost:$PORT" --dbpath "$HOME/ghost/content/data/ghost-local.db"
